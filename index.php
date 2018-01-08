5e2006c06e358f060ab8cddabda0f364a5923076
hali <hali@bell>
2018-01-08 16:44:01 +0100

    Nev es szin modositasa

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$nev = 'HGabor';
+$nev = 'Kicsi Vuk';
 $datum = new \DateTime();
 $datumStr = $datum->format('Y-m-d\TH:i:s');
-$szin = '#ff0000';
+$szin = '#dd22dd';
 
 require 'contents.php';
